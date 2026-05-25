8a3590228d7942659958ea8b08bc87f0e621182b
Luis Feliciano <justblasey@gmail.com>
2026-05-25 03:30:38 -0400

    Refactoring and adding functionality with tests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,64 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
+use mini_grep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
 }
